9fc4dce918edc6984a54629c4a8b3f5c2867a9dc
alexclp <alexandru.clapa@gmail.com>
2016-03-15 11:51:09 +0000

    Regex for first case movie title

--- src/alexmihnea/model/Film.java
+++ src/alexmihnea/model/Film.java
@@ -1,16 +1,13 @@
 package alexmihnea.model;
 
-import java.time.Year;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-/**
- * Created by alexclp on 14/03/2016.
- */
 public class Film extends Item {
     private String year;
     private String quality;
     private String title;
+
     public Film(String fileName) {
         super(fileName);
         System.out.println(fileName);
@@ -19,40 +16,29 @@ public class Film extends Item {
         extractYear();
     }
 
-    private void extractTitle(){
-        Pattern pattern1 = Pattern.compile("(\\w*\\s*)*\\w*");
-        Pattern pattern2 = Pattern.compile("\\s(\\w*\\s)*\\w*\\.");
-        Pattern pattern3 = Pattern.compile("\\s(\\w*\\s)*");
-        Pattern case1 = Pattern.compile("(\\w*\\s*)*\\w*\\(\\.+\\)\\s\\(.+\\)\\.");
-        Pattern case2 = Pattern.compile("\\(\\.*\\)\\s\\(\\.*\\)\\s\\.+");
-        Matcher case1Matcher = case1.matcher("The Genius Skater Of Paris (1990) (HD, 720p).");
-        Matcher case2Matcher = case2.matcher(fileName);
-        Matcher matcher1 = pattern1.matcher("The Genius Skater Of Paris (1990) (HD, 720p).");
-        Matcher matcher2 = pattern2.matcher(fileName);
-        Matcher matcher3 = pattern3.matcher(fileName);
-        if(matcher1.find())
-            System.out.println(matcher1.group(0));
-        if(case1Matcher.find()) {
-            System.out.println(case1Matcher.group(0));
+    private void extractTitle() {
+//        ^(?<name>[^\(]+)\s+\(
+        Pattern firstCasePattern = Pattern.compile("^(?<name>[^\\(]+)\\s+\\(");
+        Matcher firstCaseMatcher = firstCasePattern.matcher("The Genius Skater Of Paris (1990) (HD, 720p).");
+        Pattern finalTitlePattern = Pattern.compile("\\D+\\s");
+
+        if (firstCaseMatcher.find()) {
+            Matcher finalMatcher = finalTitlePattern.matcher(firstCaseMatcher.group("name"));
+            System.out.println(firstCaseMatcher.group("name"));
+            title = finalMatcher.group("name");
         }
-//            if(matcher1.find()){
-//            title = matcher1.group(0);}}
-//        } else if(case2Matcher.find()) {
-//            if(matcher2.find()){
-//            title = matcher2.group(0);}
-//        }
-// } else{
-//            title = matcher3.group(0);
-//        }
+
         System.out.println(title);
     }
+
+
     private void extractQuality() {
         Pattern pattern = Pattern.compile(".(\\p{Upper}\\p{Upper},\\s\\d+p).");
         Matcher matcher = pattern.matcher(fileName);
         if (matcher.find()) {
             Pattern pattern1 = Pattern.compile("\\p{Upper}\\p{Upper},\\s\\d+p");
             Matcher matcher1 = pattern1.matcher(matcher.group(0));
-            if(matcher1.find()){
+            if (matcher1.find()) {
                 quality = matcher1.group(0);
             }
         }
@@ -64,7 +50,7 @@ public class Film extends Item {
         if (matcher.find()) {
             Pattern pattern1 = Pattern.compile("\\d\\d\\d\\d");
             Matcher matcher1 = pattern1.matcher(matcher.group(0));
-            if(matcher1.find()){
+            if (matcher1.find()) {
                 year = matcher1.group(0);
             }
         }
